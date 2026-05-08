02f5c7f3b41940bc7c61503cfe7ca44e1b29f88f
dieguxty <cdmx3047@amerike.edu.mx>
2026-05-08 01:58:05 -0600

    Sistema de recarga y hitmarker arreglado

--- Assets/Scripts/DisparoBalas.cs
+++ Assets/Scripts/DisparoBalas.cs
@@ -1,23 +1,62 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class DisparoBalas : MonoBehaviour
 {
+    [Header("Disparo")]
     public Transform firePoint;
     public float range = 100f;
     public int damage = 25;
 
-    public Hitmarker hitmarker; // 👈 referencia al hitmarker
+    [Header("Munición")]
+    public int maxAmmo = 30;
+    public int currentAmmo;
+
+    public float reloadTime = 2f;
+    private bool isReloading = false;
+
+    [Header("UI")]
+    public TextMeshProUGUI ammoText;
+
+    [Header("Hitmarker")]
+    public Hitmarker hitmarker;
+
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+        UpdateAmmoUI();
+    }
 
     void Update()
     {
+        if (isReloading)
+            return;
+
+        // 🔫 Disparar
         if (Input.GetMouseButtonDown(0))
         {
             Shoot();
+        }
+
+        // ♻️ Recargar
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reload());
         }
     }
 
     void Shoot()
     {
+        
+        if (currentAmmo <= 0)
+        {
+            Debug.Log("Sin munición");
+            return;
+        }
+
+        currentAmmo--;
+        UpdateAmmoUI();
+
         RaycastHit hit;
 
         if (Physics.Raycast(firePoint.position, firePoint.forward, out hit, range))
@@ -30,13 +69,32 @@ public class DisparoBalas : MonoBehaviour
             {
                 zombie.TakeDamage(damage);
 
-                // 🔥 activar hitmarker
+                // 🎯 Hitmarker
                 if (hitmarker != null)
                 {
                     hitmarker.ShowHitmarker();
                 }
             }
         }
+    }
+
+    System.Collections.IEnumerator Reload()
+    {
+        isReloading = true;
+
+        Debug.Log("Recargando...");
+
+        yield return new WaitForSeconds(reloadTime);
+
+        currentAmmo = maxAmmo;
+        UpdateAmmoUI();
+
+        isReloading = false;
+    }
+
+    void UpdateAmmoUI()
+    {
+        ammoText.text = currentAmmo + " / " + maxAmmo;
     }
 }
 
